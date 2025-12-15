ea3793c6726962d54b661c652239a9b9f1643bdc
Antonio Ignacio Skorin garcía <antonio.skorin@gmail.com>
2025-12-15 00:12:28 -0300

    Feat: config para rewards

--- src/trainers/ippo_trainer.py
+++ src/trainers/ippo_trainer.py
@@ -49,6 +49,12 @@ class IPPOTrainer:
             "agent_observation": self.exp_config["agent"]["observation"],
             "normalize_reward": env_params.get("normalize_reward", False),
             "gamma": hyperparameters["gamma"],
+            # Rewards
+            "crash_vehicle_penalty": env_params.get("crash_vehicle_penalty", 5.0),
+            "crash_object_penalty": env_params.get("crash_object_penalty", 5.0),
+            "out_of_road_penalty": env_params.get("out_of_road_penalty", 5.0),
+            "speed_reward": env_params.get("speed_reward", 0.1),
+            "success_reward": env_params.get("success_reward", 10.0),
         }
 
     @staticmethod
